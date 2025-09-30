a93e7c1cc04794553a96f73bc9909783127caede
Kassandra-Olv25 <kassandra.olv.rodrigues@gmail.com>
2025-09-29 22:18:51 -0400

    feat: adicionando movimentação de jogador

--- src/classes/Player.js
+++ src/classes/Player.js
@@ -1,14 +1,20 @@
 class Player { //define a classe Player
     width; // Declara a propriedade 'width' (largura) para a classe.
 
-       constructor() {
+       constructor(canvasWidth, canvasHeight) { // Construtor da classe que inicializa as propriedades do jogador.
         this.width = 100; //define a largura do jogador
         this.height = 100;  // Define a altura do jogador
+
         this.position = {  // Cria um objeto 'position' para armazenar as coordenadas (x, y) do jogador.
-            x: 0, //define a posição inicial do jogador no eixo x (horinzontal)
-            y: 0 // Define a posição inicial no eixo Y (vertical) 
+            x: canvasWidth / 2 - this.width / 2, 
+            y: canvasHeight - this.height - 30 // Posiciona o jogador próximo à parte inferior do canvas
         }
  } 
+
+draw (ctx) { // Método 'draw' que recebe o contexto do canvas como parâmetro.
+    ctx.fillStyle = "red"; // Define a cor de preenchimento como vermelho
+    ctx.fillRect(this.position.x, this.position.y, this.width, this.height); // Desenha um retângulo representando o jogador na posição (x, y) com a largura e altura definidas.
+   }
 }
 
 export default Player; // Exporta a classe 'Player' como padrão para que possa ser importada em outros arquivos.
--- src/index.js
+++ src/index.js
@@ -7,4 +7,18 @@ const ctx = canvas.getContext("2d"); // Contexto 2D do canvas
 canvas.width = innerWidth;  // Define a largura do canvas
 canvas.height = innerHeight;  // Define a altura do canvas
 
-const player = new Player(); // Cria uma nova instância do jogador
+const player = new Player(canvas.width, canvas.height); // Cria uma nova instância do jogador
+
+player.draw(ctx); // Desenha o jogador no canvas
+
+addEventListener ("keydown", (event) => { // Adiciona um ouvinte de evento para a tecla pressionada
+    const key = event.key.toLowerCase(); // Converte a tecla pressionada para minúscula
+
+    if (key === "a") {
+        player.position.x -= 20; // Move o jogador para a esquerda
+    }
+
+    if (key === "d") {
+        player.position.x += 20; // Move o jogador para a direita
+    }   
+});
