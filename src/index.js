a713457382951140d636f9310664116cd1e3c8e5
Kassandra-Olv25 <kassandra.olv.rodrigues@gmail.com>
2025-09-29 20:06:31 -0400

    feat: configura canvas no JS e inicia loop de animação

---
+++ src/index.js
@@ -0,0 +1,6 @@
+const canvas = document.querySelector("canvas"); // Seleciona o canvas
+const ctx = canvas.getContext("2d"); // Contexto 2D do canvas
+
+
+canvas.width = innerWidth;  // Define a largura do canvas
+canvas.height = innerHeight;  // Define a altura do canvas
